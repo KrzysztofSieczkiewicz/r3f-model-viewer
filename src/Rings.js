0ca2107b8b1071daf1480f05ad8a7e1f60d4c781
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2023-05-15 15:32:19 +0200

    rings fin

--- src/Rings.js
+++ src/Rings.js
@@ -1,5 +1,6 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { Color } from "three";
 
 export function Rings() {
     const itemsRef = useRef([]);
@@ -7,8 +8,23 @@ export function Rings() {
     useFrame((state) => {
         for (let i=0; i<itemsRef.current.length; i++) {
             let mesh = itemsRef.current[i];
+
             let z = (i - 7) * 3.5;
             mesh.position.set(0, 0, -z);
+
+            let dist = Math.abs(z);
+            mesh.scale.set(1 - dist * 0.04, 1 - dist * 0.04, 1- dist * 0.04);
+
+            let colorScale = 1;
+            if (dist > 2) {
+                colorScale = 1 - (Math.min(dist, 12) - 2) / 10;
+            }
+
+            if (i%2 === 1) {
+                mesh.material.emissive = new Color(6, 0.15, 0.7).multiplyScalar(colorScale);
+            } else {
+                mesh.material.emissive = new Color(0.1, 0.7, 1).multiplyScalar(colorScale);
+            }
         }
     })
 
