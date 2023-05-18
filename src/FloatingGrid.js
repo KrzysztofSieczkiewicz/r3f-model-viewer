42100a0e6ccd6a0c9063a95bdb9a1ae3363fe2f6
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2023-05-18 15:57:23 +0200

    floating-grid init

---
+++ src/FloatingGrid.js
@@ -0,0 +1,28 @@
+import { useLoader } from "@react-three/fiber";
+import { useEffect } from "react";
+import { MeshBasicMaterial, RepeatWrapping, TextureLoader } from "three";
+
+export function FloatingGrid() {
+    const diffuse = useLoader(TextureLoader, "textures/grid-texture.png");
+
+    useEffect(() => {
+        diffuse.wrapS = RepeatWrapping;
+        diffuse.wrapT = RepeatWrapping;
+        diffuse.anisotropy = 4;
+        diffuse.repeat.set(30, 30);
+        diffuse.offset.set(0.0);
+    }, [diffuse])
+
+    return <>
+        <mesh rotation x={-Math.PI*0.5} position={[0, 0.425, 0]}>
+            <planeGeometry args={[35, 35]} />
+            <MeshBasicMaterial
+                color={[1,1,1]}
+                opacity={0.25}
+                map={diffuse}
+                alphaMap={diffuse}
+                transparent={true}
+            />
+        </mesh>
+    </>
+}
