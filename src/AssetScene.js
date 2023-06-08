8ad395b05748c1c5a95f8f38b0ca84d84e9d5ca0
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2023-06-08 18:41:39 +0200

    lights functions wip

--- src/AssetScene.js
+++ src/AssetScene.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
 
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import './style.css';
@@ -6,6 +7,15 @@ import { Asset } from './Asset';
 import { Lights } from './components/Lights';
 import { Canvas } from '@react-three/fiber';
 import { Sidebar } from './Sidebar';
+
+const defaultLight = {
+  type: "pointLight",
+  position:[5,5,0],
+  color: [0,1,1],
+  intensity: 1,
+  angle: 0.6,
+  penumbra: 0.6
+}
 
 function AssetScene() {
   const [lightsList, setLightsList] = useState([{
@@ -25,13 +35,37 @@ function AssetScene() {
     type:"pointLight"
   }]);
 
+  // Consider moving functions for lights, assets etc to their respective helpers
+  function addLight() {
+    const light = defaultLight;
+    light.id = nanoid(4);
+
+    setLightsList([...lightsList, light]);
+  }
+
+  function removeLight(id) {
+      setLightsList(lightsList.current.filter(light => light.id !== id));
+  }
+
+  function updateLight(id, light) {
+      setLightsList((lightsList) => {
+          const newLightsList = [...lightsList];
+          newLightsList[id] = light;
+      });
+  }
+
+  //addLight(); // Resolve why addLight() results in infinite render loop
+  let newLight = defaultLight;
+  newLight.color = [0,1,1];
+  newLight.position = [-5,5,0];
+
   return (
     <>
       <Canvas shadows>
         <color args={[0, 0, 0]} attach="background" />
         <OrbitControls target={[0, 0.32, 0]} maxPolarAngle={1.45} />
         <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]}/>
-        <Lights lightsList={lightsList} setLightsList={setLightsList} />
+        <Lights lightsList={lightsList}/>
         <Asset />
       </Canvas>
       <Sidebar />
--- src/components/Lights.js
+++ src/components/Lights.js
@@ -1,33 +1,5 @@
-import { nanoid } from 'nanoid';
-
-const defaultLight = {
-    type: "pointLight",
-    color: [0,1,1],
-    intensity: 1,
-    angle: 0.6,
-    penumbra: 0.6
-}
-
 export function Lights(props) {
     const lightsList = props.lightsList;
-
-    function addLight() {
-        const light = defaultLight;
-        light.id = nanoid(4);
-
-        props.setLightsList([...lightsList, light]);
-    }
-
-    function removeLight(id) {
-        props.setLightsList(lightsList.current.filter(light => light.id !== id));
-    }
-
-    function updateLight(id, light) {
-        props.setLightsList((lightsList) => {
-            const newLightsList = [...lightsList];
-            newLightsList[id] = light;
-        });
-    }
 
     return (
         <>
