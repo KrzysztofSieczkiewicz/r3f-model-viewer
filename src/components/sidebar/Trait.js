4a659aa2d76c44bc5c0101f88091e0f32150578b
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2023-07-09 22:50:30 +0200

    Trait input skeleton

--- src/components/sidebar/Trait.js
+++ src/components/sidebar/Trait.js
@@ -1,4 +1,29 @@
+import { useState } from "react";
+
 export function Trait(props) {
+    const { value, min, max, step } = props;
+
+    const [ protectedValue, setProtectedValue ] = useState(value);
+
+    function handleProtectedValue(e) {
+        // make this function to never set value over max and under min
+        // also guard value so updateLight (a.k.a. handleChange)
+        // uses only protected value
+        
+        // first suggested solution is to take input value (props.value)
+        // then prevent it from going outside of bonds
+        // then calling handleChange inside this method
+        // and finally replacing onChange call with this function
+        if (value > max) {
+            setProtectedValue(max);
+        } else if (value < min) {
+            setProtectedValue(min);
+        } else {
+            setProtectedValue(value);
+        }
+        console.log("value: ", value, ", protectedValue: ", protectedValue);
+        props.handleChange(e);
+    }
 
     //handle props types
     function handleTraitType() {
@@ -6,11 +31,14 @@ export function Trait(props) {
             return (
                 <input className="trait-input"
                     type="number"
-                    value={props.value}
-                    min={props.min}
-                    max={props.max}
-                    step={props.step}
-                    onChange={props.handleChange}
+                    value={protectedValue}
+                    min={min}
+                    max={max}
+                    step={step}
+                    onKeyUp={e => handleProtectedValue(e)}
+                    onChange={e => handleProtectedValue(e)}
+                    inputMode="numeric"
+                    pattern="[0-9]+"
                 />
             );
         }
