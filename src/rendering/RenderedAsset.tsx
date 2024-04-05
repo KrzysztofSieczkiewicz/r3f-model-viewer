b62d4be36ba78c882073f9224aec019d3c4cd348
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-04-05 11:15:07 +0200

    extending to multiple meshes

--- src/rendering/RenderedAsset.tsx
+++ src/rendering/RenderedAsset.tsx
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { useGLTF, useHelper } from "@react-three/drei";
 import { PivotControls } from "@react-three/drei/web/pivotControls";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { BoxHelper, Group, Mesh, Object3DEventMap } from "three/src/Three";
 import { AssetWrapper } from "../models/Asset";
 import React from "react";
@@ -18,46 +18,86 @@ type GLTFResult = GLTF & {
     };
   };
 
+// TODO: Consider PivotControls vs TransformControls (or maybe add a way to toggle them)
 export const RenderedAsset = ( {asset}: Props) => {
     const { updateAssetProperty } = useSidebarControlsContext();
 
     const meshRef = useRef<Mesh>(null);
     const controlsRef = useRef<Group<Object3DEventMap>>(null)
-    useHelper(meshRef as any, BoxHelper, 'cyan')
 
+    useHelper(meshRef as any, BoxHelper, 'cyan')
     const { nodes } = useGLTF("models/pear/Pear2_LOD0.gltf")  as unknown as GLTFResult;
+
 
     const handleControlsDrag = () => {
         const controlsPosition = controlsRef.current?.getWorldPosition(new THREE.Vector3);
-        const controlsRotation = controlsRef.current?.getWorldQuaternion(new THREE.Quaternion)
-        const assetPosition = meshRef.current?.getWorldPosition(new THREE.Vector3)
+        //const controlsRotation = controlsRef.current?.getWorldQuaternion(new THREE.Quaternion)
+
         updateAssetProperty(asset.id, 'position', [controlsPosition?.x, controlsPosition?.y, controlsPosition?.z])
-        updateAssetProperty(asset.id, 'rotation', [controlsRotation?.x, controlsRotation?.y, controlsRotation?.z])
+        //updateAssetProperty(asset.id, 'rotation', [controlsRotation?.x, controlsRotation?.y, controlsRotation?.z])        
     }
+
+    const handleAssetSelected = () => {
+        // TRIGGER PROPER SELECT FUNCTION (PROBABLY PASSED VIA SidebarControlsContext)
+        // IF ASSET IS SELECTED, DISPLAY THE BoxHelper
+    }
+
+    const handleAssetHovered = () => {
+        // DISPLAY SUBTLE BoxHelper
+    }
+
+    const handleDragEnd = () => {
+        const controlsPosition = controlsRef.current?.getWorldPosition(new THREE.Vector3);
+        console.log([controlsPosition?.x, controlsPosition?.y, controlsPosition?.z])
+
+        updateAssetProperty(asset.id, 'position', [controlsPosition?.x, controlsPosition?.y, controlsPosition?.z])
+        console.log(asset.position);
+    }
+        
+    useEffect( () => {
+        controlsRef.current?.position.set(...asset.position)
+        controlsRef.current?.matrixWorld.setPosition(new THREE.Vector3(...asset.position))
+    }, [asset.position])
 
     if(!asset.visible) return;
 
+    // TODO [TUTORING]: WHEN CONTROLS ARE DRAGGED DYNAMICALLY AND MOUSE BUTTON IS RELEASED CONTROLS MOVE A BIT FURTHER THAT THE MODEL ITSELF
+    // IT'LL JUMP BACK INTO POSITION WHEN ANOTHER DRAG IS INITIATED
+    // BUT EVEN onDragEnd CANNOT FIX THAT
+    // HOW TO KEEP TRACK OF THAT?
     return (
-        <PivotControls
-            onDrag={ () => { handleControlsDrag() }}
-            ref={controlsRef}
-            visible={true}
-            depthTest={false}
-            key={asset.id} 
+        <group
+                key={asset.id} 
+                position={asset.position}
         >
+            <PivotControls
+                onDrag={ () => { handleControlsDrag() }}
+                onDragEnd={ () => { handleDragEnd() }}
+                ref={controlsRef}
+                visible={true}
+                depthTest={false}
+            />
             <mesh
                 matrixWorldAutoUpdate={true}
                 ref={meshRef}
-                key={asset.id}
+                onPointerOver={() => {
+                    //console.log("Pointer moved over the mesh")
+                }}
+                onPointerOut={() => {
+                    //console.log("Pointer removed from mesh")
+                }}
+                onClick={(e) => {
+                    //updateSelected(e.intersections[0].object.assetID);
+                }} 
                 castShadow = {asset.castShadow}
                 receiveShadow = {asset.receiveShadow}
                 geometry={nodes.Aset_food_fruit_S_tezbbgrra_LOD0.geometry} // TODO: Still to be parametrized
                 material={nodes.Aset_food_fruit_S_tezbbgrra_LOD0.material} // TODO: As above
-                position={asset.position}
+                position={[0,0,0]}
                 rotation={asset.rotation}
                 scale={asset.scale}
             />
-        </PivotControls >
+        </group>
     );
 }
 
