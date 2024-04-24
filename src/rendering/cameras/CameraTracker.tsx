8add53031ff60db6e29f0eb1c499668595666c40
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-04-24 13:05:06 +0200

    SlidersArray overhaul wip 3 - CameraTracker init

--- src/rendering/cameras/CameraTracker.tsx
+++ src/rendering/cameras/CameraTracker.tsx
@@ -1,24 +1,34 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import React from "react";
+import React, { useEffect } from "react";
 import { RefObject, useState } from "react";
 import { PerspectiveCamera as PerspectiveCameraType} from 'three/src/Three';
+import { useSidebarControlsContext } from "../../components/sidebar/SidebarControlsContext";
 
 type Props = {
-    cameraRef: RefObject<PerspectiveCameraType>
+  cameraRef: RefObject<PerspectiveCameraType>
 }
 
 // TODO [TUTORING]: WHATS A PROPER PLACE FOR THIS? IT'S NEITHER UI, NOR RENDERING
 export const CameraTracker = ( { cameraRef }: Props ) => {
+  const { scene, updateScene } = useSidebarControlsContext();
 
-    const [ cameraRotation, setCameraRotation ] = useState([0,0,0])
+  const [ cameraRotation, setCameraRotation ] = useState(scene.viewCameraRotation)
 
-    useFrame( () => {
-        if(cameraRef.current) {
-          const cameraRotation = cameraRef.current.rotation;
-          setCameraRotation([cameraRotation.x, cameraRotation.y, cameraRotation.z])
-        }
-    })
+  
+  useFrame( () => {
+    if(cameraRef.current) {
+      const rotation = cameraRef.current.rotation;
+      setCameraRotation([rotation.x, rotation.y, rotation.z])
+      console.log("USE FRAME IS TRIGGERED")
+    }
+  })
 
-      return <PerspectiveCamera fov={50} position={[3, 2, 5]} />
+  // TODO [TUTORING]: IF I SET SCENE PROPERTY, ELEMENTS ARE GETTING RERENDERED (TRIGGERING USEFRAME AND IN TURN TRIGGERING USEEFFECT AGAIN)
+  useEffect( () => {
+    updateScene('viewCameraRotation', cameraRotation)
+    console.log("CAMERA UPDATED")
+  }, [])
+
+  return <PerspectiveCamera fov={50} position={[3, 2, 5]} />
 }
