16c077d204abd3fb7889f8c606e3bf1be1c126a1
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-05-25 17:12:18 +0200

    Ambient light rework

--- src/components/canvas/scene/AmbientLight.tsx
+++ src/components/canvas/scene/AmbientLight.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { useSceneValue } from "../../contexts/SceneContext";
 
 export const AmbientLight = () => {
+    const [ isAmbientActive ] = useSceneValue((scene) => scene['isAmbientActive']);
     const [ ambientColor ] = useSceneValue((scene) => scene['ambientColor']);
     const [ ambientIntensity ] = useSceneValue((scene) => scene['ambientIntensity']);
 
     return (
-        <ambientLight 
-            color={ambientColor} 
-            intensity={ambientIntensity} 
-        />
+        <>
+            {isAmbientActive && 
+                <ambientLight 
+                    color={ambientColor} 
+                    intensity={ambientIntensity} />
+            }
+        </>
     );
 }
