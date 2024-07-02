352bf059a49635e6bbcb4684b3ad677f3b7b7e1b
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-07-02 22:33:07 +0200

    component children mapping for multiline traits

--- src/components/sidebar/commons/traitContainers/MultilineTraits.tsx
+++ src/components/sidebar/commons/traitContainers/MultilineTraits.tsx
@@ -7,7 +7,19 @@ type Props = {
 // TODO: if name is provided display it in the first column (centered)
 // Then display all provided children in the second column
 // Then, if provided -> display helper lines and "lock" button in the third column
-export const MultilineTraits = () => {
+export const MultilineTraits = ({children}: Props) => {
+
+    const render = () => {
+        React.Children.map(children, (child) => {
+            <tr>
+                <label></label>
+                {child}
+                <div></div>
+            </tr>
+        })
+    }
+
+
     return (
         <></>
     );
