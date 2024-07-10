708aad6a724170d810a0902bb6dedf20f0f99aee
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-07-10 21:38:45 +0200

    MultilineSlidersNumeric with slider locking

--- src/components/sidebar/controls/MultilineSlidersNumeric.tsx
+++ src/components/sidebar/controls/MultilineSlidersNumeric.tsx
@@ -1,26 +1,26 @@
-import React, { ReactNode, useEffect, useRef, useState } from "react";
-import styles from './MultilineTraits.module.css';
-import { AxesLockButton } from "../../controls/buttons/AxesLockButton";
+import React, { cloneElement, ReactElement, ReactNode, useEffect, useRef, useState } from "react";
+import styles from './MultilineSlidersNumeric.module.css';
+import { AxesLockButton } from "./buttons/AxesLockButton";
+import { normalizeArrayByIndex } from "../../../utils/mathUtil";
+import { SliderNumeric } from "./SliderNumeric";
 
-type Props = {
-    displayName: string;
-    children: ReactNode;
+type Props<T> = {
+    values: {property: (keyof T), value: number}[];
+    handleChange: (change: Partial<T>) => void;
+    displayName: string
 }
 
-// TODO: if name is provided display it in the first column (centered)
-// Then display all provided children in the second column
-// Then, if provided -> display helper lines and "lock" button in the third column
-export const MultilineTraits = ({displayName, children}: Props) => {
+export const MultilineSlidersNumeric = <T,>({displayName, values, handleChange}: Props<T>) => {
 
-    const [rowHeight, setRowHeight] = useState(0);
     const [startingPointYOffset, setStartingPointOffset] = useState(0);
-
-    const [isLocked, setIsLocked] = useState(false);
+    const [rowHeight, setRowHeight] = useState(0);
+    const [isLocked, setIsLocked] = useState(true);
 
     const gridContainerRef = useRef<HTMLDivElement>(null);
 
-    const ROWS_NUMBER = React.Children.count(children);
+    const ROWS_NUMBER = values.length;
 
+    // Handles component size and children number
     useEffect(() => {
         if(!gridContainerRef.current) return;
 
@@ -32,15 +32,13 @@ export const MultilineTraits = ({displayName, children}: Props) => {
         setStartingPointOffset(calcStartingPointOffset);
     }, [])
 
-    const generateLinePath = (index: number) => {
+    const generateGuideLinePath = (index: number) => {
         // Determine index distance from the middle
         const avgIndex= Math.round(ROWS_NUMBER) / 2;
         const dist = avgIndex - index - 0.5;
-
         // Declare line lengths
         const horizontalLineLength = 18 + Math.abs(2 * dist);
         const verticalLineHeight = dist * rowHeight;
-
         // Determine starting points
         const startPointX = 0;
         const startPointY = index * rowHeight + startingPointYOffset;
@@ -59,23 +57,48 @@ export const MultilineTraits = ({displayName, children}: Props) => {
         );
     }
 
+    const handleSliderChange= (index: number, newValue: number) => {
+        const rawValues = values.map(entry => entry.value);
+        let change: Partial<T>;
+
+        if(isLocked) {
+            const scales = normalizeArrayByIndex(rawValues, rawValues[index]);
+            const normalizedValues = rawValues.map( (value, i) => {
+                return value+(newValue-rawValues[index]) * scales[i];
+            });
+            change = normalizedValues.reduce((acc, value, i) => {
+                const entry = values[i];
+                acc[entry.property] = value as any;
+                return acc;
+            }, {} as Partial<T>);
+        } else {
+            change = { [values[index].property]: newValue } as Partial<T>;
+        }
+        handleChange(change);
+    }
+
     // TODO: ADD A VERTICAL LINE TO MARK ALL RELATED TRAITS
     return (
         <>
             <label className={styles.containerName}>{displayName}</label>
             <div ref={gridContainerRef} className={styles.gridContainer}>
                 <div className={styles.column1}>
-                    {React.Children.map(children, (child) => {
+                    {values.map((entry, index) => {
                         return (
-                            <div>{child}</div>
-                        );
+                        <SliderNumeric 
+                            key={index} 
+                            step={0.01} 
+                            rounding={0} 
+                            value={entry.value} 
+                            handleChange={(val) => handleSliderChange(index, val)} />
+                        )
                     })}
                 </div>
 
                 <div className={styles.column2}>
-                    {React.Children.map(children, (child, index) => {
+                    {values.map((entry, index) => {
                         return (
-                                <> {generateLinePath(index)} </>
+                                <> {generateGuideLinePath(index)} </>
                         ); 
                     })}
                     <div className={styles.buttonContainer}>
