a1ee1ff6e903f1d10ac87313b0c09481a8357b22
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-08-15 22:03:16 +0200

    Physical Material with possible Textures for each map

--- src/models/assets/materials/MappedMaterial.ts
+++ src/models/assets/materials/MappedMaterial.ts
@@ -1,9 +1,11 @@
-import { Texture } from "three";
+import { Texture, Vector2 } from "three";
 import { BaseMaterialProperties } from "./BaseMaterial";
 
+// TODO: REPLACE ALL COLORS TYPES WITH ThreeJS 'Color' class (consider that - it provides some functions, but they might be optional)
 // TODO: Maybe introduce simplified materials with only few maps and sliders
 // AND advanced materials with more advanced maps
-export type MappedMaterialsProperties = {
+// TODO: Consider replacing Physical and Standard materiales with mapped counterparts -> if there is map provided, hide (maybe grey out?) other controls for that property
+export type MappedStandardMaterialsProperties = {
     alphaMap: Texture,
     ambientOcclusionMap: Texture,
     ambientOcclusionMapIntensity: number,
@@ -12,7 +14,7 @@ export type MappedMaterialsProperties = {
     bumpScale: number,
 
     color: string,
-    colorMap: Texture, // in ThreeJs it's named just 'map'
+    colorMap: Texture, // in ThreeJS it's named just 'map'
 
     displacementMap: Texture,
     displacementScale: number,
@@ -36,3 +38,45 @@ export type MappedMaterialsProperties = {
     roughnessMap: Texture,
     roughness: number,
 } & BaseMaterialProperties
+
+export type MappedPhysicalMaterialsProperties = {
+    anisotropyMap: Texture | null,
+    anisotropy: number,
+    anisotropyRotation: number,
+
+    attenuationColor: string,
+    attenuationDistance: number,
+    
+    clearcoatMap: Texture | null,
+    clearcoat: number,
+    clearcoatRoughnessMap: Texture | null,
+    clearcoatRoughness: number,
+    clearcoatNormalMap: Texture | null,
+    clearcoatNormalScale: Vector2,
+
+    dispersion: number,
+    ior: number,
+    
+    reflectivity: number,
+
+    iridescenceMap: Texture | null,
+    iridescence: number,
+    iridescenceIor: number,
+
+    sheen: number,
+    sheenRoughnessMap: Texture | null,
+    sheenRoughness: number,
+    sheenColorMap: string,
+    sheenColor: string,
+
+    specularIntensityMap: Texture | null,
+    specularIntensity: number,
+    specularColorMap: Texture | null,
+    specularColor: string,
+
+    thicknessMap: number,
+    thickness: number,
+
+    transmissionMap: Texture | null,
+    transmission: number,
+} & MappedStandardMaterialsProperties
