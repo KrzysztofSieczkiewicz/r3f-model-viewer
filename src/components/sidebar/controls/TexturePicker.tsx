dd004170538888210289433a5e2778e14bfa93f1
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-08-15 23:15:37 +0200

    texture picker wip

--- src/components/sidebar/controls/TexturePicker.tsx
+++ src/components/sidebar/controls/TexturePicker.tsx
@@ -1,4 +1,35 @@
+import React, { useState } from "react";
+import { Texture } from "three";
 
-export const TexturePicker = () => {
-    
+type Props = {
+    map: Texture;
+}
+
+export const TexturePicker = ({map}: Props) => {
+
+    const [ isPickerOpen, setIsPickerOpen ] = useState(false);
+    const [ position, setPosition ] = useState(0);
+
+    const toggleColorPicker = (e: React.MouseEvent<HTMLDivElement>) => {
+        setPosition(e.clientX - 25);
+        setIsPickerOpen(active => !active);
+    }
+
+    return (<>
+        <div onMouseDown={(e) => toggleColorPicker(e)} >
+            <p>{map.name}</p>
+        </div>
+
+        {isPickerOpen &&
+        <div>
+            <PickerWindow />
+        </div>}
+    </>)
+}
+
+// TODO: CONSIDER REPLACING WITH MODAL?
+const PickerWindow = () => {
+    return (<>
+
+    </>)
 }
