b03392b9a3b1a7873c516475ee298410b109f606
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-09-14 00:08:32 +0200

    reworking api client structure wip

--- src/api/sceneManagerApi/SceneManagerApi.ts
+++ src/api/sceneManagerApi/SceneManagerApi.ts
@@ -1,3 +1,37 @@
 import { ApiClient } from "../ApiClient";
 
+// TODO: replace this by getting this from centralized env variables
+const BASE_URL = "localhost:9090";
+
+const defaultHeaders = {
+    'Content-Type': 'application/json',
+} as Record<string, string>
+
+const request = async <T>(
+    path: string,
+    method: "GET" | "POST" | "PUT" | "DELETE",
+    headers?: Record<string, string>,
+    body?: any,
+): Promise<T> => {
+    const url = `${BASE_URL}${path}`;
+    const bodyString = body ? JSON.stringify(body) : "";
+
+    const options: RequestInit = {
+        method,
+        headers: {...defaultHeaders, ...headers },
+        body: bodyString,
+    };
+
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new Error(`${method} request failed with status ${response.status}.\nError message: ${response.statusText}`)
+    }
+
+    return response.json() as Promise<T>
+}
+
+const get = () => {
+    
+}
+
 export const apiClient = new ApiClient('localhost:9090');
