c9728976a3b8bd3e30daab9074ae42d9f9c10654
kristoff <krzysztof.sieczkiewicz@gmail.com>
2025-03-13 11:07:49 +0100

    updated ReactDOMClient in the index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import { App } from './App';
 
-ReactDOM.render(
+const root = ReactDOMClient.createRoot( document.getElementById('root') );
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
