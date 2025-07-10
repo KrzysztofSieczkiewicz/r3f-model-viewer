1993b8d702eb29b8e5a7f7258f7041393ae1255a
kristoff <krzysztof.sieczkiewicz@gmail.com>
2025-07-10 15:47:32 +0200

    wip working on modal resizing

--- src/features/modals/assets/AddAssetModal.tsx
+++ src/features/modals/assets/AddAssetModal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import styles from './AddAssetModal.module.css';
 
 import { ReactComponent as SphereIcon } from '../../../icons/sidebar/primitives/primitive_sphere.svg'
@@ -8,59 +8,80 @@ import { AssetModalPrimitivesList } from "./AssetModalPrimitivesList";
 import { ButtonLargeRectangle } from "../common/ButtonLargeRectangle";
 import { ImportMeshModal } from "./ImportMeshModal";
 import { ButtonBackRound } from "../common/ButtonBackRound";
-import { useTransition, animated, easings } from "react-spring";
+import { useTransition, animated, easings, useSpring } from "react-spring";
+import { useElementSize } from "../../../hooks/useElementSize";
 
 type Props = {
     closeModal: () => void
 }
 
+type PageState = 'main' | 'primitives' | 'import';
+
 export const AddAssetModal = ({closeModal}: Props) => {
 
-    const [currentContent, setCurrentContent] = useState<ReactNode|null>(null);
+    const [currentPage, setCurrentPage] = useState<PageState>('main');
+    const [measuredContainerWidth, setMeasuredContainerWidth] = useState(0);
+    const [measuredContainerHeight, setMeasuredContainerHeight] = useState(0);
 
+    const [mainPageContentRef, mainPageSize] = useElementSize<HTMLDivElement>();
+    const [primitivesContentRef, primitivesPageSize] = useElementSize<HTMLDivElement>();
+    const [importDetailsContentRef, importDetailsPageSize] = useElementSize<HTMLDivElement>();
 
-    // TODO: finish with proper conditions -> experiment with that
-    const transition = useTransition(true, {
-        from: { opacity: 0, maxWidth: '30vw' },
-        enter: { opacity: 1, maxWidth: '60vw' },
-        leave: { opacity: 0, maxWidth: '0vw' },
+    const activePageSize = 
+        currentPage === 'main' ? mainPageSize :
+        currentPage === 'primitives' ? primitivesPageSize :
+        importDetailsPageSize;
+    
+    const modalSpring = useSpring({
+        width: '50vw',//ctivePageSize.width,
+        height: '50vh',//activePageSize.height,
+        config: { duration: 250, easing: easings.easeInOutQuad }
+    });
+    
+    const contentTransition = useTransition(currentPage, {
+        from: { opacity: 0 },    //from: { opacity: 0, transform: 'translateX(100%)' },
+        enter: { opacity: 1 },   //enter: { opacity: 1, transform: 'translateX(0%)' },
+        leave: { opacity: 0 },   //leave: { opacity: 0, transform: 'translateX(-100%)' },
         config: { duration: 250, easing: easings.easeInOutQuad },
+        exitBeforeEnter: false,
     });
 
 
     const switchToPrimitivesList = () => {
-        setCurrentContent( renderPrimitivesList() );
+        setCurrentPage('primitives');
     }
 
     const switchToImportDetails = () => {
-        setCurrentContent( renderImportDetails() )
+        setCurrentPage('import');
     }
 
     const switchToMainPage = () => {
-        setCurrentContent( null )
+        setCurrentPage('main');
     }
 
 
     const renderPrimitivesList = () => {
-        return (<>
-            <div className={styles.topBar}>
-                <ButtonBackRound onClick={switchToMainPage}/>
+        return (
+            <div ref={primitivesContentRef} className={styles.pageContent}>
+                <div className={styles.topBar}>
+                    <ButtonBackRound onClick={switchToMainPage}/>
+                </div>
+                <AssetModalPrimitivesList closeModal={closeModal} />
             </div>
-            <AssetModalPrimitivesList closeModal={closeModal} />
-        </>)
+        )
     }
 
     const renderImportDetails = () => {
-        return (<>
+        return (<div ref={importDetailsContentRef} className={styles.pageContent}>
             <div className={styles.topBar}>
                 <ButtonBackRound onClick={switchToMainPage}/>
             </div>
             <ImportMeshModal src={""} closeModal={() => {}}/>
-        </>)
+        </div>)
     }
 
     const renderMainPage = () => {
-        return (<>
+        return (<div ref={mainPageContentRef} className={styles.pageContent}>
             <section className={styles.pageSection}>
                 <h3 className={styles.sectionTitle}>
                     Browse models
@@ -85,22 +106,24 @@ export const AddAssetModal = ({closeModal}: Props) => {
                 </h3>
                 <p> Something will be here later </p>
             </section>
-        </>);
+        </div>);
     }
 
 
-    return (
-        transition( (style, item) => 
-            item 
-                ? <animated.div 
-                    style={style}
-                    className={styles.modalContents} >
-                    
-                    {currentContent || renderMainPage()}
-
+    return (<>
+        <animated.div
+            style={modalSpring}
+            className={styles.modalContents}
+        >
+            <div className={styles.contentsContainerPlaceholder}  />        
+            {/* {contentTransition((style,item) => (
+                <animated.div style={{ ...style, position: 'absolute', width: '100%', height: '100%' }}>
+                    {item === 'main' && renderMainPage()}
+                    {item === 'primitives' && renderPrimitivesList()}
+                    {item === 'import' && renderImportDetails()}
                 </animated.div>
-                : null
-        )
-    );
+            ))} */}
+        </animated.div>
+    </>);
 
 }
---
+++ src/hooks/useElementSize.ts
@@ -0,0 +1,43 @@
+import { useCallback, useLayoutEffect, useRef, useState } from "react"
+
+
+type Size = {
+    width: number,
+    height: number,
+}
+
+export const useElementSize = <T extends HTMLElement = HTMLDivElement>(): [React.RefObject<T>, Size ] => {
+    const ref = useRef<T>(null);
+    const [size, setSize] = useState<Size>({width: 0, height: 0});
+
+
+    const updateSize = useCallback(() => {
+        if (!ref.current) return;
+
+        setSize({
+            width: ref.current.offsetWidth,
+            height: ref.current.offsetHeight
+        });
+    }, [])
+
+
+    useLayoutEffect(() => {
+        const element = ref.current;
+        if (!element) return;
+
+        updateSize();
+
+        const observer = new ResizeObserver((entries) => {
+            if (entries[0] && entries[0].target === element) {
+                updateSize();
+            }
+        });
+        observer.observe(element);
+
+        return () => {
+            observer.unobserve(element);
+        };
+    }, [updateSize]);
+
+    return [ref, size];
+}
